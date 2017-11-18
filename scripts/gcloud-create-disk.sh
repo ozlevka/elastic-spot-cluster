0dae81d6b2d3bce4a2eee40cd88fd57edfd86aad
Lev Ozeryansky <ozlevka@gmail.com>
2017-11-18 11:11:01 +0200

    Append zone variable

--- scripts/gcloud-create-disk.sh
+++ scripts/gcloud-create-disk.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
 
+if [ -z "$CLUSTER_ZONE" ]; then
+    CLUSTER_ZONE="us-east1-c"
+fi
+
+
 gcloud compute \
     --project="ozlevka" \
     disks create "disk-for-1" \
-    --zone="us-central1-f" \
+    --zone="$CLUSTER_ZONE" \
     --type="pd-standard" \
     --size=50GB
